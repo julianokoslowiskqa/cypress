3641503cb4c58030291abd327a7c98d5384ed067
julianokoslowiskqa <julianokoslowisk@gmail.com>
2023-06-28 16:22:26 -0300

    Reddme

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return require('./cypress/plugins/index.js')(on, config)
     },
   },
-});
+})
